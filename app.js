ec2cd2375c7af712cf4dd366a7d2547559bc290a
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro.local>
2023-01-30 23:28:13 +0100

    js file-webcomponents structure

---
+++ app.js
@@ -0,0 +1,19 @@
+class MasonryLayout extends HTMLElement {
+    constructor() {
+      super();
+      const shadow = this.attachShadow({ mode: 'open' });
+      const template = document.createElement('template');
+      template.innerHTML = `
+        <style>
+          p {
+            color: red;
+          }
+        </style>
+        <p>Hello World!</p>
+      `;
+      shadow.appendChild(template.content.cloneNode(true));
+    }
+  }
+  
+  customElements.define('my-component', MasonryLayout);
+  
