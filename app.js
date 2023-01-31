86bfe2a87a9d10631b9ede6b2e43cacce3f46d5e
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro.local>
2023-01-31 16:54:45 +0100

    content

--- app.js
+++ app.js
@@ -8,22 +8,21 @@ class MasonryLayout extends HTMLElement {
         <style>
           .grid {
             position: relative;
-            max-width: 100%;
+            max-width: 100%; 
             height: 100vh;
             display: grid;
             grid-template-columns: repeat(auto-fill, minmax(300px));
             grid-template-rows: minmax(100px, auto);
-            margin:  0 40px;
+            margin:   40px;
             grid-auto-flow: dense;
             grid-gap: 10px;
+            
           }
 
-          .grid .grid-item:hover {
-            background: #1ebae9;
-        }
+       
         
           
-          .grid-item {
+          .grid .grid-item {
             background: #333;
             padding: 20px;
             display: grid;
@@ -32,11 +31,31 @@ class MasonryLayout extends HTMLElement {
             text-align: center;
             color: #fff;
             transition: 0.5s;
+            
+            
+          }
+          .grid .grid-item:hover {
+            background: #1ebae9;
+        }
+
+        .grid .grid-item .content {
+          padding: 10px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+        }
+
+          .grid-item img {
+            max-width: 100px;
+            margin-bottom: 10px;
+            position: relative;
           }
 
           #grid-item-1 {
             grid-column: span 2;
-            grid-row: span 1;
+            grid-row: span 2;
         }
         #grid-item-2 {
             grid-column: span 1;
@@ -66,31 +85,59 @@ class MasonryLayout extends HTMLElement {
         <div class="grid">
         
         <div class="grid-item" id="grid-item-1">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-2">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-3">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-4">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title4</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-5">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 5</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-6">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 6</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         <div class="grid-item" id="grid-item-7">
-        <p><slot name="desc"/></p>
+        <div class="content">
+        <img src="./images/maurice-sahl-AIRK6woHruo-unsplash.jpg" alt="">
+        <h3>Title 7</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel nam accusamus obcaecati dolor nisi mollitia odio explicabo recusandae error enim soluta assumenda? Laboriosam nisi modi delectus culpa doloremque voluptatibus!</p>
+        </div>
         </div>
 
         </div>
